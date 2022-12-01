9e326eacdfa49ba1db68f918250c0e5b85e45161
cosmos1030 <dyk6208@gmail.com>
2022-12-01 05:50:38 +0000

    static

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("myapp.urls")),
+    path('blog/', include("blog.urls")),
     path('challenges/', include("challenges.urls")),
     path('admin/', admin.site.urls),
 ]
