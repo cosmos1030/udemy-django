3e322de8e3552815d3071cbdf8d630c715db4341
cosmos1030 <dyk6208@gmail.com>
2022-12-06 14:36:28 +0000

    model

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .models import Book
+
 # Create your views here.
 def index(request):
-    return render()
+    books = Book.objects.all()
+    return render(request, 'book_outlet/index.html', {
+        "books": books
+    })
