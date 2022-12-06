c583038e692e8fe5b96e132634edfa26cfea4a3f
cosmos1030 <dyk6208@gmail.com>
2022-12-06 13:41:19 +0000

    template

--- blog/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-mydic = {1: 'first post', 2: 'second post', 3: 'third post'}
-
-# Create your views here.
-def index(request):
-    return render(request, 'blog/index.html',
-    {'mydic': mydic})
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("myapp.urls")),
-    path('blog/', include("blog.urls")),
-    path('challenges/', include("challenges.urls")),
     path('admin/', admin.site.urls),
 ]
